d10d01ed7694869229eabbc6daeac3289c36d13f
saeedkamyab <saeed.kamyab@yahoo.com>
2024-05-12 12:43:48 +0330

    i made first version

--- MyFileLibrary/MainWindow.xaml.cs
+++ MyFileLibrary/MainWindow.xaml.cs
@@ -25,33 +25,106 @@ namespace MyFileLibrary
 
         private void BtnScan_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                Scan();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+        void Scan()
+        {
             drives = DriveInfo.GetDrives();
             foreach (DriveInfo drive in drives)
             {
-               DriveList.Items.Add(drive);
+                DriveList.Items.Add(drive);
+                if (drive.IsReady)
+                {
+
+                    string[] directories = Directory.GetDirectories(drive.RootDirectory.FullName);
+                    DriveInfoList.Items.Clear();
+                    foreach (string directory in directories)
+                    {
+                        DriveInfoList.Items.Add(directory);
+                    }
+
+                    string[] files = Directory.GetFiles(drive.RootDirectory.FullName);
+                    InfoList.Items.Clear();
+                    foreach (string file in files)
+                    {
+                        InfoList.Items.Add(file);
+                    }
+
+
+                    //-----------------------
+                    string[] directories2 = Directory.GetDirectories(drive.RootDirectory.FullName);
+                    //DriveInfoList.Items.Clear();
+
+                    Mylist.Items.Clear();
+                    foreach (string directory in directories)
+                    {
+                        try
+                        {
+                            var items = Directory.GetFiles(directory);
+                            if (items != null)
+                                foreach (var i in items)
+                                {
+                                    Mylist.Items.Add(i);
+                                }
+                        }
+                        catch (Exception ex)
+                        {
+                            File.AppendAllText(@"C:\Users\saeid\Desktop\Log.txt", "\n" + ex.Message);
+                            continue;
+                        }
+
+
+
+                        //DriveInfoList.Items.Add(directory);
+                    }
+
+                }
             }
 
-       }
 
+            List<string> li = new List<string>();
+
+            foreach (var i in Mylist.Items)
+            {
+                li.Add(i.ToString());
+            }
+            //foreach (var s in li)
+            //{
+            //    if (li.Contains(s))
+            //    {
+            //        Mylist2.Items.Add(s);
+            //    }
+            //}
+        }
         private void DriveList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var d = drives[DriveList.SelectedIndex];
-            if (d.IsReady)
-            {
-                string[] directories = Directory.GetDirectories(d.RootDirectory.FullName);
-                DriveInfoList.Items.Clear();
-                foreach (string directory in directories)
-                {
-                    DriveInfoList.Items.Add(directory);
-                }
-                string[] files = Directory.GetFiles(d.RootDirectory.FullName);
-               InfoList.Items.Clear();  
-                foreach (string file in files)
-                {
-                    InfoList.Items.Add(file);
-                }
-            }
-            else { MessageBox.Show("Drive is not ready"); }
+            //var d = drives[DriveList.SelectedIndex];
+            //if (d.IsReady)
+            //{
+            //    string[] directories = Directory.GetDirectories(d.RootDirectory.FullName);
+            //    DriveInfoList.Items.Clear();
+            //    foreach (string directory in directories)
+            //    {
+            //        DriveInfoList.Items.Add(directory);
+            //    }
+            //    string[] files = Directory.GetFiles(d.RootDirectory.FullName);
+            //   InfoList.Items.Clear();  
+            //    foreach (string file in files)
+            //    {
+            //        InfoList.Items.Add(file);
+            //    }
+            //}
+            //else { MessageBox.Show("Drive is not ready"); }
         }
         private void InfoList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
